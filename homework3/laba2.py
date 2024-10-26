444867e60bed8b36cdd8a0e855e165c001cd4a16
holodae <bqcrystal75@gmail.com>
2024-10-26 17:39:00 +0300

    Добавление проверки

--- homework3/laba2.py
+++ homework3/laba2.py
@@ -2,12 +2,16 @@ from datetime import datetime
 
 def calculate_age(birthdate_str):
     
-    birthdate = datetime.strptime(birthdate_str, "%d/%m/%Y")
-    today = datetime.today()
+    try:
+        birthdate = datetime.strptime(birthdate_str, "%d/%m/%Y")
+        today = datetime.today()
+        
     
-    age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
-    
-    return age
+        age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+        return age
+
+    except ValueError:
+        return "Ошибка: Неверный формат даты. Пожалуйста, введите дату в формате дд/мм/гггг."
 
 birthdate_input = input("Введите вашу дату рождения (дд/мм/гггг): ")
 age = calculate_age(birthdate_input)
